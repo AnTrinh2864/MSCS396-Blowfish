671dae321d4b63dc042004134d96d7d133fc6f41
AnTrinh2864 <118484971+AnTrinh2864@users.noreply.github.com>
2024-04-04 19:38:02 -0500

    Update DES.java

--- DES.java
+++ DES.java
@@ -1,67 +1,74 @@
-package desTest;
-import java.util.*;
+
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 import java.io.*;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 class DES{
-   public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
-      
-   	int j=100;
-    	long[] encTime= new long[j];
-   	long[] decTime= new long[j];
-   	
-       //String we want to encrypt
-       String  message="This is a confidential message.";
-       byte[] myMessage =message.getBytes(); //string to byte array as DES works on bytes
-       //If you want to use your own key
-       // SecretKeyFactory MyKeyFactory = SecretKeyFactory.getInstance("DES");
-       // String Password = "My Password";
-       // byte[] mybyte =Password.getBytes();
-       // DESKeySpec myMaterial = new DESKeySpec(mybyte);
-       // SecretKey myDESKey = MyKeyFactory.generateSecret(myMaterial);
-       KeyGenerator Mygenerator = KeyGenerator.getInstance("DES");
-       SecretKey myDesKey = Mygenerator.generateKey();
-      
-       //initializing crypto algorithm
-       Cipher myCipher = Cipher.getInstance("DES");
-      
-       for(int i =0; i<j;i++) {
-       	long startTime = System.nanoTime();
-           //Generating Key
-          
-         
-           //setting encryption mode
-           myCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
-           byte[] myEncryptedBytes=myCipher.doFinal(myMessage);
-          
-           long estimatedTime = System.nanoTime() - startTime;
-   		System.out.println("Encryption took " + estimatedTime * 1000/1e9 + " in miliseconds.");
-   		encTime[i]=estimatedTime;
-   		long startTime2 = System.nanoTime();
-           //setting decryption mode
-           myCipher.init(Cipher.DECRYPT_MODE, myDesKey);
-           byte[] myDecryptedBytes=myCipher.doFinal(myEncryptedBytes);
-          
-           long estimatedTime2 = System.nanoTime() - startTime2;
-   		System.out.println("decryption took " + estimatedTime2 * 1000/1e9  + " in miliseconds.");
-   		decTime[i]=estimatedTime2;
-           //print message in byte format
-           //System.out.println(Arrays.toString(myEncryptedBytes));
-           //System.out.println(Arrays.toString(myDecryptedBytes));
-           String encrypteddata=new String(myEncryptedBytes);
-           String decrypteddata=new String(myDecryptedBytes);
-           System.out.println("Message : "+ message);
-           System.out.println("Encrypted - "+ encrypteddata);
-           System.out.println("Decrypted Message - "+ decrypteddata);
-   	}//end for
-   }
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+
+        int j=100;
+        long[] encTime= new long[j];
+        long[] decTime= new long[j];
+
+        //String we want to encrypt
+        String  message="This is a confidential message.";
+        byte[] myMessage =message.getBytes(); //string to byte array as DES works on bytes
+        //If you want to use your own key
+        // SecretKeyFactory MyKeyFactory = SecretKeyFactory.getInstance("DES");
+        // String Password = "My Password";
+        // byte[] mybyte =Password.getBytes();
+        // DESKeySpec myMaterial = new DESKeySpec(mybyte);
+        // SecretKey myDESKey = MyKeyFactory.generateSecret(myMaterial);
+        KeyGenerator Mygenerator = KeyGenerator.getInstance("DES");
+        SecretKey myDesKey = Mygenerator.generateKey();
+
+        //initializing crypto algorithm
+        Cipher myCipher = Cipher.getInstance("DES");
+
+        for(int i =0; i<j;i++) {
+            long startTime = System.nanoTime();
+            //Generating Key
+
+
+            //setting encryption mode
+            myCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
+            byte[] myEncryptedBytes=myCipher.doFinal(myMessage);
+
+            long estimatedTime = System.nanoTime() - startTime;
+            System.out.println("DES encryption took " + estimatedTime * 1000/1e9 + " in miliseconds.");
+            encTime[i]=estimatedTime;
+            long startTime2 = System.nanoTime();
+            encrypt_lib(message);
+            long estimatedTime2 = System.nanoTime() - startTime2;
+            System.out.println("Blowfish encryption took " + estimatedTime2 * 1000/1e9  + " in miliseconds.");
+            decTime[i]=estimatedTime2;
+            //print message in byte format
+            //System.out.println(Arrays.toString(myEncryptedBytes));
+            //System.out.println(Arrays.toString(myDecryptedBytes));
+
+        }//end for
+    }
+
+    public static String encrypt_lib (String plaintext){
+        try {
+            String key = "AAFFAAFFBBCCDDBB";
+            byte[] keyData = (key).getBytes();
+            SecretKeySpec secretKeySpec = new SecretKeySpec(keyData, "Blowfish");
+            Cipher cipher = Cipher.getInstance("Blowfish");
+            cipher.init(Cipher.ENCRYPT_MODE,secretKeySpec);
+            byte[] hasil = cipher.doFinal(plaintext.getBytes());
+            return new String(Base64.getEncoder().encode(hasil));
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
